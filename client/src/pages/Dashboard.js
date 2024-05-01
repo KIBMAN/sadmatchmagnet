b56f0d020d55d1b554d9c451b05b3fdb578f8ddd
KIBMAN <izakzoddy321@gmail.com>
2024-05-02 00:28:02 +0600

    initial matchmaking without interest

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -7,7 +7,8 @@ import axios from "axios";
 const Dashboard = () => {
 
   const [user,setUser] = useState(null)
-  const [genderedUsers,setGenderedUsers]
+  const [genderedUsers,setGenderedUsers]=useState(null)
+  const [lastDirection, setLastDirection] = useState();
   const [cookies,setCookie,removeCookie]=useCookies(['user'])
 
   const userId=cookies.UserId
@@ -37,35 +38,13 @@ const Dashboard = () => {
   useEffect(()=>{
     getUser()
     getGenderedUsers()
-  },[])
+  },[user,genderedUsers])
 
-  console.log('user',user)
+  console.log(genderedUsers)
 
   
 
-  const characters = [
-    {
-      name: "Richard Hendricks",
-      url: "https://imgs.search.brave.com/G7PMeUgzG1zh75C7cIXIVWlrcnUczSU8lKlLqkCC16w/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTMw/OTMyODgyMy9waG90/by9oZWFkc2hvdC1w/b3J0cmFpdC1vZi1z/bWlsaW5nLW1hbGUt/ZW1wbG95ZWUtaW4t/b2ZmaWNlLmpwZz9z/PTYxMng2MTImdz0w/Jms9MjAmYz1rUHZv/Qm02cUNZelFYTUFu/OUpVdHFMUkVYZTkt/UGxaeU1sOWktaWJh/VnVZPQ",
-    },
-    {
-      name: "Erlich Bachman",
-      url: "https://imgs.search.brave.com/G7PMeUgzG1zh75C7cIXIVWlrcnUczSU8lKlLqkCC16w/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTMw/OTMyODgyMy9waG90/by9oZWFkc2hvdC1w/b3J0cmFpdC1vZi1z/bWlsaW5nLW1hbGUt/ZW1wbG95ZWUtaW4t/b2ZmaWNlLmpwZz9z/PTYxMng2MTImdz0w/Jms9MjAmYz1rUHZv/Qm02cUNZelFYTUFu/OUpVdHFMUkVYZTkt/UGxaeU1sOWktaWJh/VnVZPQ",
-    },
-    {
-      name: "Monica Hall",
-      url: "https://imgs.search.brave.com/G7PMeUgzG1zh75C7cIXIVWlrcnUczSU8lKlLqkCC16w/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTMw/OTMyODgyMy9waG90/by9oZWFkc2hvdC1w/b3J0cmFpdC1vZi1z/bWlsaW5nLW1hbGUt/ZW1wbG95ZWUtaW4t/b2ZmaWNlLmpwZz9z/PTYxMng2MTImdz0w/Jms9MjAmYz1rUHZv/Qm02cUNZelFYTUFu/OUpVdHFMUkVYZTkt/UGxaeU1sOWktaWJh/VnVZPQ",
-    },
-    {
-      name: "Jared Dunn",
-      url: "https://imgs.search.brave.com/G7PMeUgzG1zh75C7cIXIVWlrcnUczSU8lKlLqkCC16w/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTMw/OTMyODgyMy9waG90/by9oZWFkc2hvdC1w/b3J0cmFpdC1vZi1z/bWlsaW5nLW1hbGUt/ZW1wbG95ZWUtaW4t/b2ZmaWNlLmpwZz9z/PTYxMng2MTImdz0w/Jms9MjAmYz1rUHZv/Qm02cUNZelFYTUFu/OUpVdHFMUkVYZTkt/UGxaeU1sOWktaWJh/VnVZPQ",
-    },
-    {
-      name: "Dinesh Chugtai",
-      url: "https://imgs.search.brave.com/G7PMeUgzG1zh75C7cIXIVWlrcnUczSU8lKlLqkCC16w/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTMw/OTMyODgyMy9waG90/by9oZWFkc2hvdC1w/b3J0cmFpdC1vZi1z/bWlsaW5nLW1hbGUt/ZW1wbG95ZWUtaW4t/b2ZmaWNlLmpwZz9z/PTYxMng2MTImdz0w/Jms9MjAmYz1rUHZv/Qm02cUNZelFYTUFu/OUpVdHFMUkVYZTkt/UGxaeU1sOWktaWJh/VnVZPQ",
-    },
-  ];
-  const [lastDirection, setLastDirection] = useState();
+  
 
   const swiped = (direction, nameToDelete) => {
     console.log("removing: " + nameToDelete);
@@ -80,18 +59,18 @@ const Dashboard = () => {
     <>
     {user &&
     <div className="dashboard">
-      <ChatContainer user={user}/>
+       <ChatContainer user={user}/>
       <div className="swipe-container">
         <div className="card-container">
-          {characters.map((character) => 
+          {genderedUsers?.map((genderedUser) => 
             <TinderCard // Change here: Replace TinderCard with div
               className="swipe"
-              key={character.name}
-              onSwipe={(dir) => swiped(dir, character.name)}
-              onCardLeftScreen={() => outOfFrame(character.name)}
+              key={genderedUser.first_name}
+              onSwipe={(dir) => swiped(dir, genderedUser.first_name)}
+              onCardLeftScreen={() => outOfFrame(genderedUser.first_name)}
             >
-              <div style={{ backgroundImage: 'url('+character.url+')'}} className="card"  >
-                <h3>{character.name}</h3>
+              <div style={{ backgroundImage: 'url('+genderedUser.url+')'}} className="card"  >
+                <h3>{genderedUser.first_name}</h3>
               </div>
             </TinderCard>
           )}
